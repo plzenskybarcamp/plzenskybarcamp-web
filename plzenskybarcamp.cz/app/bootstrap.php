e4479b5113a650b411b2f41756b1b86798f316f6
Jakub Bouček <pan@jakubboucek.cz>
2014-02-10 08:49:57 +0100

    Rebuild to Nette

---
+++ plzenskybarcamp.cz/app/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+use Nette\Application\Routers\Route;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+
+//$configurator->setDebugMode(TRUE);  // debug mode MUST NOT be enabled on production server
+$configurator->enableDebugger(__DIR__ . '/../log');
+
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->addDirectory(__DIR__ . '/../vendor/others')
+	->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+$container = $configurator->createContainer();
+
+Route::$defaultFlags |= ($container->httpRequest->isSecured() ? Route::SECURED : 0);
+
+return $container;
