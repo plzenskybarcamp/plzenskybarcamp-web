bce4b1e4bf2d3b0602af7d5ade8ac54d7ff29213
Jakub Bouček <pan@jakubboucek.cz>
2014-03-12 20:41:08 +0100

    Save user ID as string (rspt. keep last var-type)

--- plzenskybarcamp.cz/app/components/registration/Identity.php
+++ plzenskybarcamp.cz/app/components/registration/Identity.php
@@ -6,14 +6,33 @@ use Nette\Security as NS;
 
 class Identity extends NS\Identity implements IIDentity, NS\IIdentity {
 
+	private $id;
 	private $isRegistered = FALSE;
 	private $isSpeaker = FALSE;
+
+	/**
+	* Overriding Nette method (we need string of id)
+	**/
+	public function setId($id)
+	{
+		$this->id = $id;
+		return $this;
+	}
+
+	/**
+	* Overriding Nette method
+	**/
+	public function getId()
+	{
+		return $this->id;
+	}
 
 	/**
 	* Temporary method for testing
 	**/
 	public function setRegistered( $value ) {
 		$this->isRegistered = $value;
+		return $this;
 	}
 
 	/**
@@ -21,6 +40,7 @@ class Identity extends NS\Identity implements IIDentity, NS\IIdentity {
 	**/
 	public function setSpeaker( $value ) {
 		$this->isSpeaker = $value;
+		return $this;
 	}
 
 	public function isRegistered() {
