44f108a8d560fcbd61421aea743480aacc095891
Jakub Bouček <pan@jakubboucek.cz>
2014-03-21 09:43:55 +0100

    Set e-mail for Debugger's logs

--- plzenskybarcamp.cz/app/bootstrap.php
+++ plzenskybarcamp.cz/app/bootstrap.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode(TRUE);  // debug mode MUST NOT be enabled on production server
-$configurator->enableDebugger(__DIR__ . '/../log');
+$configurator->enableDebugger(__DIR__ . '/../log', 'pan@jakubboucek.cz');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 if(!file_exists(__DIR__ . '/../temp/sessions')) mkdir(__DIR__ . '/../temp/sessions');
