d3d8b7c77d62cab430322408e648d569647063e9
Jakub Bouček <pan@jakubboucek.cz>
2014-03-26 16:49:01 +0100

    Fix CSV users format (now compatible with Mailchimp and Excel, LibreOffice and others)

--- plzenskybarcamp.cz/app/AdminModule/presenters/Users.php
+++ plzenskybarcamp.cz/app/AdminModule/presenters/Users.php
@@ -25,30 +25,35 @@ use Nette,
 
         ob_start();
         $df = fopen("php://output", 'w');
-        fputcsv($df, array("Jméno", "E-mail", "Registrace"), ";", '"');
+        fputcsv($df, array("E-mail", "Jméno", "Příjmení", "Registrace"), ",", '"');
         foreach ($users as $user) {
+            $names = explode(" ", $user['name'], 2);
+            $fname = $names[0];
+            $lname = (isset($names[1])?$names[1]:"");
             @fputcsv($df, array(
-                $user['name'],
                 $user['email'],
+                $fname,
+                $lname,
                 ( $user['created_date'] ? date( 'Y-m-d H:i:s',  $user['created_date']->sec) : NULL )
-            ), ";", '"');
+            ), ",", '"');
         }
         fclose($df);
         $csv = ob_get_clean();
 
         $now = gmdate("D, d M Y H:i:s");
+        $fileDatePostfix = gmdate("Ymd.his");
         header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
         header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
         header("Last-Modified: {$now} GMT");
 
-        // force download  
+        // force download
         header("Content-Type: application/force-download");
         header("Content-Type: application/octet-stream");
         header("Content-Type: application/download");
         header("Content-Length: " . strlen($csv));
 
         // disposition / encoding on response body
-        header("Content-Disposition: attachment;filename=users.csv");
+        header("Content-Disposition: attachment;filename=users-$fileDatePostfix.csv");
         header("Content-Transfer-Encoding: binary");
         echo $csv;
         $this->terminate();
