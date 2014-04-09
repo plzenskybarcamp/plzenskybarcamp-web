9aace2c54f3f23a78b15b92131bf5c0bfdd26efb
Jakub Bouček <pan@jakubboucek.cz>
2014-04-09 18:51:45 +0200

    Lepší link na PDF

--- plzenskybarcamp.cz/app/presenters/ProgramPresenter.php
+++ plzenskybarcamp.cz/app/presenters/ProgramPresenter.php
@@ -15,4 +15,10 @@ class ProgramPresenter extends BasePresenter
 
 	}
 
+	public function actionPdf( )
+	{
+		$response = new \Nette\Application\Responses\FileResponse( __DIR__.'/../../www/files/program_06_04.pdf', 'program.pdf', 'application/pdf' );
+		$this->sendResponse( $response );
+	}
+
 }
