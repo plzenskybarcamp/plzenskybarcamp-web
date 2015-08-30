77c9ed424609fb3ca9a510b12439992e469a4f83
Jakub Bouček <pan@jakubboucek.cz>
2015-08-31 01:30:52 +0200

    Revert "Test CDN77"
    This reverts commit ef47bdd546e9d03d38e42d112283bf2e033dced1.

--- plzenskybarcamp.cz/www/index.php
+++ plzenskybarcamp.cz/www/index.php
@@ -5,34 +5,4 @@
 
 $container = require __DIR__ . '/../app/bootstrap.php';
 
-function replacecdn($buffer)
-{
-  // replace all the apples with oranges
-  return (str_replace(
-  		[
-  			'd32gpbkjyl921a.cloudfront.net',
-  			'href="/css/',
-  			'src="/media/',
-  			'src="/js/',
-  			'src="/images/',
-  		],
-  		[
-  			'1009227059.rsc.cdn77.org',
-  			'href="https://1166919629.rsc.cdn77.org/css/',
-  			'src="https://1166919629.rsc.cdn77.org/media/',
-  			'src="https://1166919629.rsc.cdn77.org/js/',
-  			'src="https://1166919629.rsc.cdn77.org/images/',
-  		], $buffer));
-}
-
-$amazon = isset($_GET['cdn']) && $_GET['cdn'] == 'amazon';
-
-if(!$amazon) {
-	ob_start("replacecdn");
-}
-
 $container->getService('application')->run();
-
-if(!$amazon) {
-	ob_end_flush();
-}
