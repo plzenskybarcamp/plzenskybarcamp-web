1227b18ed40348afc0f42e42e4cc8fcde74355bc
Jakub Bouček <pan@jakubboucek.cz>
2016-02-23 11:38:48 +0100

    Fix deploy script

--- plzenskybarcamp.cz/deploy.sh
+++ plzenskybarcamp.cz/deploy.sh
@@ -6,13 +6,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SSH="ssh sbl"
 
 echo "Uploading data to SSH…"
-rsync -rcP --delete --exclude-from=".rsync-exclude" "${DIR}/" "sbl:$REMOTE_DIR/"
+rsync -rcP --delete --exclude-from="${DIR}/.rsync-exclude" "${DIR}/" "sbl:$REMOTE_DIR/"
 
 echo "Replace file permissions…"
 $SSH sudo fixwww.sh $REMOTE_DIR
 
 echo "Remove temporary files…"
-$SSH find $REMOTE_DIR/temp -mindepth 2 -type f -delete
+#$SSH find $REMOTE_DIR/temp -mindepth 2 -type f -delete
 
 echo -n "Remove nette email-sent marker… "
 ssh sbl /bin/bash << EOF
