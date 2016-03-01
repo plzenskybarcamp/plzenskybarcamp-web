3deadecb7a51bc6c5b91d524e493d8f99f865ccc
Jakub Bouček <pan@jakubboucek.cz>
2016-03-01 15:00:28 +0100

    Admin - fix copy VIP link to clipboard

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,3 +1,7 @@
 //@koala-prepend "libs/jquery-2.1.3.js"
 //@koala-prepend "libs/bootstrap-3.1.1.js"
 //@koala-prepend "libs/netteForms.js"
+
+$(window).load(function(){
+	new Clipboard('.clipboard');
+});
