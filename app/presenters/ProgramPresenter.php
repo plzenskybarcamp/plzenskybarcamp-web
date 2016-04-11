9f8d8cd413d1ae495698bb535b9bcd9d5cefc1c3
Jakub Bouček <pan@jakubboucek.cz>
2016-04-11 22:55:11 +0200

    Program 2016 (+ remove old local-stored program)

--- app/presenters/ProgramPresenter.php
+++ app/presenters/ProgramPresenter.php
@@ -18,10 +18,4 @@ class ProgramPresenter extends BasePresenter
 
 	}
 
-	public function actionPdf( )
-	{
-		$response = new \Nette\Application\Responses\FileResponse( __DIR__.'/../../www/files/program.pdf', 'program.pdf', 'application/pdf' );
-		$this->sendResponse( $response );
-	}
-
 }
