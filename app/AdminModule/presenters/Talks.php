5381d6ca399d917803a1534c21669b22e0ab51db
Jakub Bouček <pan@jakubboucek.cz>
2016-04-15 04:23:51 +0200

    Revert "Add more fields to CSV exports" as needless.
    This reverts commit a8da879c7235d2c6137c142ccc96231bd22ff0fd.

--- app/AdminModule/presenters/Talks.php
+++ app/AdminModule/presenters/Talks.php
@@ -28,21 +28,15 @@ use Nette,
 
         ob_start();
         $df = fopen("php://output", 'w');
-        fputcsv($df, array("ID", "Název", "Speaker", "Bio", "Photo", "E-mail", "Web", "Popis", "Pro koho je určena", "Od", "Do", "Kde"), ",", '"');
+        fputcsv($df, array("ID", "Název", "Speaker", "E-mail", "Popis", "Pro koho je určena"), ",", '"');
         foreach ($talks as $talk) {
             @fputcsv($df, array(
                 $talk['_id'],
                 $talk['title'],
                 $talk['speaker']['name'],
-                $talk['speaker']['bio'],
-                $talk['speaker']['picture_url'],
                 $talk['speaker']['email'],
-                $talk['speaker']['web'],
                 $talk['description'],
                 $talk['purpose'],
-                $talk['time_from'],
-                $talk['time_to'],
-                $talk['place'],
             ), ",", '"');
         }
         fclose($df);
