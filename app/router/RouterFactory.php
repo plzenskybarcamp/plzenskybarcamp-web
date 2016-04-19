d77c54418613dc68b141da6701edb081f0bcc008
Jakub Bouček <pan@jakubboucek.cz>
2016-04-19 15:29:26 +0200

    Remove unused page (/kudy-kam), change URL to prepare archivation.

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -43,15 +43,20 @@ class RouterFactory
 	public function createHttpRouter()
 	{
 		$router = new RouteList();
-		$router[] = new Route('partneri', 'Homepage:partners');
-		$router[] = new Route('kudy-kam', 'Homepage:location');
-		$router[] = new Route('informace', 'Homepage:info');
-		$router[] = new Route('napsali-o-nas', 'Homepage:written');
-		$router[] = new Route('promo-team', 'Homepage:promoTeam');
-		$router[] = new Route('kontakt', 'Homepage:contact');
+		$router[] = new Route('2016/partneri', 'Homepage:partners');
+		$router[] = new Route('partneri', 'Homepage:partners', Route::ONE_WAY);
+		$router[] = new Route('2016/informace', 'Homepage:info');
+		$router[] = new Route('informace', 'Homepage:info', Route::ONE_WAY);
+		$router[] = new Route('2016/napsali-o-nas', 'Homepage:written');
+		$router[] = new Route('napsali-o-nas', 'Homepage:written', Route::ONE_WAY);
+		$router[] = new Route('2016/promo-team', 'Homepage:promoTeam');
+		$router[] = new Route('promo-team', 'Homepage:promoTeam', Route::ONE_WAY);
+		$router[] = new Route('2016/kontakt', 'Homepage:contact');
+		$router[] = new Route('kontakt', 'Homepage:contact', Route::ONE_WAY);
 		$router[] = new Route('profil', 'Conference:profil');
-		$router[] = new Route('ucastnici', 'Conference:visitors');
-		$router[] = new Route('prednasky', 'Conference:talks');
+		$router[] = new Route('2016/ucastnici', 'Conference:visitors');
+		$router[] = new Route('ucastnici', 'Conference:visitors', Route::ONE_WAY);
+		$router[] = new Route('prednasky', 'Conference:talks', Route::ONE_WAY);
 		$router[] = new Route('prednasky/zebricek', 'Conference:talksRanking');
 		$router[] = new Route('2016/prednasky', 'Conference:talks', Route::ONE_WAY);
 		$router[] = new Route('2016/prednasky/<talkId>', 'Conference:talksDetail');
@@ -59,7 +64,8 @@ class RouterFactory
 		$router[] = new Route('plzenakovo-slovnicek-pojmu', 'Homepage:vocabulary');
 		$router[] = new Route('privacy-policy', 'Homepage:privacyPolicy');
 		$router[] = new Route('terms', 'Homepage:terms');
-		$router[] = new Route('program', 'Program:list');
+		$router[] = new Route('2016/program', 'Program:list');
+		$router[] = new Route('program', 'Program:list', Route::ONE_WAY);
 		$router[] = new Route('login', 'Sign:in');
 		$router[] = new Route('logout', 'Sign:out');
 		$router[] = new Route('login/facebook', 'Sign:inFb');
