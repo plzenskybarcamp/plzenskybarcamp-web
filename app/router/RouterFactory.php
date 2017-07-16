877f9c932175c2206ea06ca2def3cfb384eac96c
Jakub Bouček <pan@jakubboucek.cz>
2017-07-16 18:58:59 +0200

    Add archive for 2017 and prepare 2018 (for #9)

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -43,27 +43,27 @@ class RouterFactory
 	public function createHttpRouter()
 	{
 		$router = new RouteList();
-		$router[] = new Route('2017', 'Homepage:default', Route::ONE_WAY);
-		$router[] = new Route('2017/partneri', 'Homepage:partners', Route::ONE_WAY);
+		$router[] = new Route('2018', 'Homepage:default', Route::ONE_WAY);
+		$router[] = new Route('2018/partneri', 'Homepage:partners', Route::ONE_WAY);
 		$router[] = new Route('partneri', 'Homepage:partners');
-		$router[] = new Route('2017/informace', 'Homepage:info', Route::ONE_WAY);
+		$router[] = new Route('2018/informace', 'Homepage:info', Route::ONE_WAY);
 		$router[] = new Route('informace', 'Homepage:info');
-		$router[] = new Route('2017/napsali-o-nas', 'Homepage:written', Route::ONE_WAY);
+		$router[] = new Route('2018/napsali-o-nas', 'Homepage:written', Route::ONE_WAY);
 		$router[] = new Route('napsali-o-nas', 'Homepage:written');
-		$router[] = new Route('2017/kontakt', 'Homepage:contact', Route::ONE_WAY);
+		$router[] = new Route('2018/kontakt', 'Homepage:contact', Route::ONE_WAY);
 		$router[] = new Route('kontakt', 'Homepage:contact');
 		$router[] = new Route('profil', 'Conference:profil');
-		$router[] = new Route('2017/ucastnici', 'Conference:visitors');
+		$router[] = new Route('2018/ucastnici', 'Conference:visitors');
 		$router[] = new Route('ucastnici', 'Conference:visitors', Route::ONE_WAY);
 		$router[] = new Route('prednasky', 'Conference:talks', Route::ONE_WAY);
-		$router[] = new Route('2017/prednasky/zebricek', 'Conference:talksRanking');
-		$router[] = new Route('2017/prednasky', 'Conference:talks');
-		$router[] = new Route('2017/prednasky/<talkId>', 'Conference:talksDetail');
+		$router[] = new Route('2018/prednasky/zebricek', 'Conference:talksRanking');
+		$router[] = new Route('2018/prednasky', 'Conference:talks');
+		$router[] = new Route('2018/prednasky/<talkId>', 'Conference:talksDetail');
 		$router[] = new Route('prednasky/<talkId>', 'Conference:talksDetail', Route::ONE_WAY);
 		$router[] = new Route('plzenakovo-slovnicek-pojmu', 'Homepage:vocabulary');
 		$router[] = new Route('privacy-policy', 'Homepage:privacyPolicy');
 		$router[] = new Route('terms', 'Homepage:terms');
-		$router[] = new Route('2017/program', 'Program:list');
+		$router[] = new Route('2018/program', 'Program:list');
 		$router[] = new Route('program', 'Program:list', Route::ONE_WAY);
 		$router[] = new Route('login', 'Sign:in');
 		$router[] = new Route('logout', 'Sign:out');
@@ -75,6 +75,7 @@ class RouterFactory
 		$router[] = new Route('2014[/<path .+>]', 'Archive:2014');
 		$router[] = new Route('2015[/<path .+>]', 'Archive:2015');
 		$router[] = new Route('2016[/<path .+>]', 'Archive:2016');
+		$router[] = new Route('2017[/<path .+>]', 'Archive:2017');
 
 		$router[] = new Route('no-track', 'Homepage:noTrack');
 		$router[] = new Route('s/<key [a-z0-9]+>[-<utm [a-z0-9]+>]', 'Shortlink:go');
