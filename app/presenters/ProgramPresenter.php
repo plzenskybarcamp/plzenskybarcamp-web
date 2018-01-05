11383a55bdb2e1f54b2a61ce79255dff73401286
Jakub Bouček <pan@jakubboucek.cz>
2018-01-05 22:05:55 +0100

    Prepare to 2018 - improves copy. closes #11

--- app/presenters/ProgramPresenter.php
+++ app/presenters/ProgramPresenter.php
@@ -11,7 +11,7 @@ class ProgramPresenter extends BasePresenter
     protected function startup()
     {
         parent::startup();
-        $this->flashMessage('Omlouváme se, program ještě není připraven');
+        $this->flashMessage('Omlouváme se, program ještě není připraven. Finální program zveřejníme 19. března 2018.');
         $this->redirect(301, ':Homepage:default');
     }
 
