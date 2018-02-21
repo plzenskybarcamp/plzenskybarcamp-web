e3ed2ed6402d665e7bf0773b791b3e9b2198b90f
Jakub Bouček <pan@jakubboucek.cz>
2018-02-21 03:23:48 +0100

    Update admin JS assets

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,7 +1,3 @@
-//@koala-prepend "libs/jquery-2.1.3.js"
-//@koala-prepend "libs/bootstrap-3.1.1.js"
-//@koala-prepend "libs/netteForms.js"
-
-$(window).load(function(){
+$(function(){
 	new Clipboard('.clipboard');
 });
