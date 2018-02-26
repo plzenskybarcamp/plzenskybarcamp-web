2b90f5fb8137d8b7d2f863113fbdc2ff3786c134
Jakub Bouček <pan@jakubboucek.cz>
2018-02-26 13:00:09 +0100

    Change JS files generator from Koalaapp to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,10 +12,36 @@ module.exports = function(grunt) {
 					"www/css/print.css": "www/less/print.less"
 				}
 			}
-		}
-	});
+		},
+        uglify: {
+            options: {
+                sourceMap: false,
+                beautify: false
+            },
+            default: {
+                files: {
+                    'www/js/main.min.js': [
+                        "www/js/libs/jquery-2.1.3.js",
+						"www/js/libs/jquery.scrollTo-1.4.14.js",
+						"www/js/libs/jquery.smooth-scroll-1.5.5.js",
+						"www/js/libs/jquery.tooltipster-3.3.0.js",
+						"www/js/libs/jquery.lazyload-rev-d14e809.js",
+						"www/js/libs/lightbox-2.7.1.js",
+						"www/js/libs/netteForms.js",
+                        "www/js/main.js"
+                    ],
+                    'www/js/admin.min.js': [
+                        "www/js/libs/netteForms.js",
+                        "www/js/admin.js"
+                    ]
+                }
+            }
+        },
+
+    });
 
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['less']);
+	grunt.registerTask('default', ['less','uglify']);
 };
