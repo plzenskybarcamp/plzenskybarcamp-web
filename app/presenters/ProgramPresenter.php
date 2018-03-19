d000233ff5fbdb598810efebcf65384beb08be30
Petr Landsman <landiik@gmail.com>
2018-03-19 20:25:39 +0100

    Display "program" page

--- app/presenters/ProgramPresenter.php
+++ app/presenters/ProgramPresenter.php
@@ -11,8 +11,6 @@ class ProgramPresenter extends BasePresenter
     protected function startup()
     {
         parent::startup();
-        $this->flashMessage('Omlouváme se, program ještě není připraven. Finální program zveřejníme 19. března 2018.');
-        $this->redirect(301, ':Homepage:default');
     }
 
 
