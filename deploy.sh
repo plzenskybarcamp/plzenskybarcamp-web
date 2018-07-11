f1b4ae3c93df58e20f57e567fbf83afe6555e6b0
Jakub Bouček <pan@jakubboucek.cz>
2018-07-11 21:49:40 +0200

    Migrate from sbl to mlh server

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,19 @@
 REMOTE_DIR="/var/www/plzenskybarcamp.cz/www"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SSH="ssh sbl"
+SSH="ssh mlh"
 
 echo "Uploading data to SSH…"
-rsync -rcP --delete --exclude-from="${DIR}/.rsync-exclude" "${DIR}/" "sbl:$REMOTE_DIR/"
+rsync -rcP --delete --exclude-from="${DIR}/.rsync-exclude" "${DIR}/" "mlh:$REMOTE_DIR/"
 
 echo "Replace file permissions…"
-$SSH sudo fixwww.sh $REMOTE_DIR
+$SSH sudo fixwww $REMOTE_DIR
 
 echo "Remove temporary files…"
 $SSH find $REMOTE_DIR/temp/cache -mindepth 2 -type f -delete
 
 echo -n "Remove nette email-sent marker… "
-ssh sbl /bin/bash << EOF
+ssh mlh /bin/bash << EOF
 	if [ -f ${REMOTE_DIR}/log/email-sent ]
 	then
 		rm ${REMOTE_DIR}/log/email-sent
